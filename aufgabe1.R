2abc32fd14b3ca59823b51f722731fe18e404d5c
Seitz Michael (seitzmi1) <seitzmi1@students.zhaw.ch>
2022-10-17 09:57:14 +0200

    Prepared for presentation
    Added library dplyr for additional operations like groupby
    Changed file path
    Added the code for the lineplot

--- aufgabe1.R
+++ aufgabe1.R
@@ -1,4 +1,7 @@
-homicide_data <- read.csv('./data/homicide-reports.csv')
+library(dplyr)
+
+# read .csv file
+homicide_data <- read.csv('C:/Users/Seitz/OneDrive/Dokumente/Studium/ExpD/Semesterarbeit/homicide_data.csv')
 
 # mindestes 6 Variablen (Spalten)
 
@@ -22,7 +25,19 @@ unique(homicide_data$Crime.Solved)
 homicide_data$Victim.Age
 homicide_data$Perpetrator.Age
 homicide_data$Year
+homicide_data$Victim.Count
+homicide_data$Perpetrator.Count
 
 # bestimmen Sie die Datentypen von allen Variablen (Spalten)
 str(homicide_data)
 
+# remove scientific notation
+options(scipen=999)
+
+# group by year and sum up incidents
+df_grp_year <- homicide_data %>% group_by(Year) %>% summarise(total_incidents = sum(Incident))
+
+# plot the data to a lineplot
+plot(df_grp_year$Year, df_grp_year$total_incidents, main = "Total Homicides in the US",xlab="Year",
+     ylab="Incident count")
+lines(df_grp_year$Year, df_grp_year$total_incidents)
